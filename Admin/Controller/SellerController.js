c68eaffca7f2206116c8266c899b6edd56f098f9
AhadBawani <ahadbawani123@gmail.com>
2023-01-28 18:30:22 +0530

    again minor changes

--- Admin/Controller/SellerController.js
+++ Admin/Controller/SellerController.js
@@ -4,22 +4,24 @@ const Seller = require('../Schemas/SellerSchema');
 
 module.exports.GET_ALL_SELLER = (async (req, res) => {
     try {
-        SellerSchema.aggregate([
-            {
-                $lookup: {
-                    from: 'sellerreviews',
-                    localField: '_id',
-                    foreignField: 'sellerId',
-                    as: 'reviews'
-                }
-            }
-        ]).then(result => {
-            console.log(result);
-            res.send('console');
-            // result will have an array of sellers with reviews
-        }).catch(error => {
-            console.log(error);
-        });
+        let seller = await Seller.find().select('_id sellerName sellerImage date');
+        res.status(200).json(seller);        
+        // SellerSchema.aggregate([
+        //     {
+        //         $lookup: {
+        //             from: 'sellerreviews',
+        //             localField: '_id',
+        //             foreignField: 'sellerId',
+        //             as: 'reviews'
+        //         }
+        //     }
+        // ]).then(result => {
+        //     console.log(result);
+        //     res.send('console');
+        // result will have an array of sellers with reviews
+        // }).catch(error => {
+        //     console.log(error);
+        // });
     }
     catch (err) {
         console.log(err)
@@ -33,24 +35,24 @@ module.exports.ADD_SELLER = (async (req, res) => {
             sellerImage: req.file.filename,
             date: req.body.date
         }).save();
-        
+
         seller
-        .then(response => {
-            if(response){
-                res.status(201).json({
-                    message: "Seller Created Successfully!",
-                    seller: {
-                        _id: seller._id,
-                        sellerName: seller.sellerName,
-                        sellerImage: seller.sellerImage,
-                        date: seller.date
-                    }
-                })
-            }
-        })
-        .catch(error => {
-            res.status(400).send(error);
-        });
+            .then(response => {
+                if (response) {
+                    res.status(201).json({
+                        message: "Seller Created Successfully!",
+                        seller: {
+                            _id: seller._id,
+                            sellerName: seller.sellerName,
+                            sellerImage: seller.sellerImage,
+                            date: seller.date
+                        }
+                    })
+                }
+            })
+            .catch(error => {
+                res.status(400).send(error);
+            });
     }
     catch (err) {
         res.send(err)
