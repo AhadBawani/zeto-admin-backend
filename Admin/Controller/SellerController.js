bf4def2762a88d1a18cd64830a7930cce719f77c
AhadBawani <ahadbawani123@gmail.com>
2023-02-13 20:20:02 +0530

    seller route issue

--- Admin/Controller/SellerController.js
+++ Admin/Controller/SellerController.js
@@ -11,10 +11,7 @@ module.exports.GET_ALL_SELLER = (async (req, res) => {
 
         for (let i = 0; i < seller.length; i++) {
             const sellerReview = await SellerReviewSchema.find({ sellerId: seller[i]?._id }).populate('userId', 'email username phoneNumber');
-            const sellerProduct = await ProductSchema.find({ sellerID: seller[i]?._id })
-                .select('productName productImage price mrp discount disabled description sellerID categoryId subCategoryId')
-                .populate('categoryId', 'category')
-                .populate('subCategoryId', 'categoryId subCategory');
+            const sellerProduct = await (await ProductSchema.find({ sellerID: seller[i]?._id }).select('_id productName productImage price mrp sellerID subCategoryId categoryId description disabled discount').populate('categoryId', 'category').populate('subCategoryId', 'subCategory'));
             if (sellerReview.length > 0) {
                 let total = 0;
                 sellerReview.map((item) => {
