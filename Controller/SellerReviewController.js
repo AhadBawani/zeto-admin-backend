7142e2839774bdddc0ef702668739afabba03387
AhadBawani <ahadbawani123@gmail.com>
2023-03-01 18:57:34 +0530

    seller review changes

--- Controller/SellerReviewController.js
+++ Controller/SellerReviewController.js
@@ -3,10 +3,10 @@ const utils = require('../Admin/AdminUtils/Common/CommonUtils');
 module.exports.GET_ALL_REVIEW = (async (req, res) => {
     try {
         const SellerReview = await SellerReviewSchema
-            .find({ deleteReview: false })
+            .find()
             .populate('userId', '_id username phoneNumber email')
             .populate('sellerId', '_id sellerImage sellerName date')
-            .select('_id sellerId userId rating review');
+            .select('_id sellerId userId rating review deleteReview');
         res.status(200).json(SellerReview);
     }
     catch (error) {
