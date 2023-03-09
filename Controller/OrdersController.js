704995a458511a4a2810118a2208b345ec13cbf7
AhadBawani <ahadbawani123@gmail.com>
2023-03-09 21:13:19 +0530

    order controller minor changes

--- Controller/OrdersController.js
+++ Controller/OrdersController.js
@@ -111,18 +111,11 @@ module.exports.DELETE_ORDER = (async (req, res) => {
         })
 })
 
-module.exports.PLACE_ORDER = (async (req, res) => {    
+module.exports.PLACE_ORDER = (async (req, res) => {
     const { userId, product, block, room, paymentType, orderDelivered } = req.body;
     var date = req.body.date;
     const today = new Date();
-    // const transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: process.env.EMAIL,
-    //         pass: process.env.PASSWORD
-    //     }
-    // });
-    // const productArr = [];
+
     const currentHour = today.getHours();
     if ((currentHour > process.env.LAST_ORDER_TIME) | (currentHour == process.env.LAST_ORDER_TIME && today.getMinutes() > 1)) {
         const tomorrow = new Date(today);
@@ -133,7 +126,7 @@ module.exports.PLACE_ORDER = (async (req, res) => {
         date = date;
     }
     try {
-        await User.findById(userId)
+        User.findById(userId)
             .exec()
             .then(userResponse => {
                 if (userResponse) {
@@ -163,10 +156,6 @@ module.exports.PLACE_ORDER = (async (req, res) => {
                                                             orderDelivered: orderDelivered,
                                                             date: date,
                                                         }).save();
-
-                                                        // UserCartSchema.findOneAndRemove({ productId: product[i].productId })
-                                                        // .exec()
-                                                        // .then(response => console.log(response));
                                                     })
                                                     .catch(error => {
                                                         res.status(400).send(error);
@@ -182,28 +171,6 @@ module.exports.PLACE_ORDER = (async (req, res) => {
                                             res.status(400).send(error);
                                         })
                                 }
-                                // const mailOptions = {
-                                //     from: 'ahadbawani123@gmail.com',
-                                //     to: userResponse.email,
-                                //     subject: 'Order Confirmation',
-                                //     text: `Dear customer,
-                                //     Thank you for your order. Your order ID is ${(response.orderId + 1)}
-                                //     your order will be delivered on ${date} between 7pm to 9pm
-
-                                //     Thank you for shopping with us
-
-
-                                //     Best regards,
-                                //     Zetomart`
-                                // };
-                                // transporter.sendMail(mailOptions, (error, info) => {
-                                //     if (error) {
-                                //         console.log(error);
-                                //     }
-                                //     else {
-                                //         console.log("Email sended successfully");
-                                //     }
-                                // })
                                 res.status(200).send({
                                     orderId: (response.orderId + 1),
                                     message: "Order placed successfully!"
@@ -231,10 +198,6 @@ module.exports.PLACE_ORDER = (async (req, res) => {
                                                             orderDelivered: orderDelivered,
                                                             date: date,
                                                         }).save();
-
-                                                        // UserCartSchema.findOneAndRemove({ productId: product[i].productId })
-                                                        // .exec()
-                                                        // .then(response => console.log(response));
                                                     })
                                                     .catch(error => {
                                                         res.status(400).send(error);
@@ -250,29 +213,6 @@ module.exports.PLACE_ORDER = (async (req, res) => {
                                             res.status(400).send(error);
                                         })
 
-                                    // const mailOptions = {
-                                    //     from: 'ahadbawani123@gmail.com',
-                                    //     to: userResponse.email,
-                                    //     subject: 'Order Confirmation',
-                                    //     text: `Dear customer,
-                                    //         Thank you for your order. Your order ID is ${process.env.START_ORDER}
-                                    //         your order will be delivered on ${date} between 7pm to 9pm
-
-                                    //         Thank you for shopping with us
-
-
-                                    //         Best regards,
-                                    //         Zetomart`
-                                    // };
-                                    // transporter.sendMail(mailOptions, (error, info) => {
-                                    //     if (error) {
-                                    //         console.log(error);
-                                    //     }
-                                    //     else {
-                                    //         console.log("Email sended successfully")
-                                    //     }
-                                    // })
-
                                     res.status(200).send({
                                         orderId: parseInt(process.env.START_ORDER),
                                         message: "Order placed successfully!"
@@ -283,15 +223,6 @@ module.exports.PLACE_ORDER = (async (req, res) => {
                         .catch(error => {
                             console.log(error);
                         })
-
-                    UserCartSchema.deleteMany({ userId: userId })
-                        .exec()
-                        .then(response => console.log(response));
-                }
-                else {
-                    res.status(400).send({
-                        message: "User not found!"
-                    })
                 }
             })
             .catch(error => {
