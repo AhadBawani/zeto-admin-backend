1fb20b5a80aaf350a358d165ed91d2df53201155
AhadBawani <ahadbawani123@gmail.com>
2023-03-13 19:50:27 +0530

    review delete changes

--- Controller/SellerReviewController.js
+++ Controller/SellerReviewController.js
@@ -13,3 +13,37 @@ module.exports.GET_ALL_REVIEW = (async (req, res) => {
         console.log(error);
     }
 })
+
+module.exports.DELETE_REVIEW = (async (req, res) => {
+    const { userId, reviewId } = req.params;
+    try {
+        utils.VERIFY_USER(userId)
+            .then((response) => {
+                if (response) {
+                    SellerReviewSchema.findByIdAndUpdate(reviewId, { deleteReview:true }, { new : true })
+                        .exec()
+                        .then(response => {                            
+                            if(response){
+                                res.status(200).send({
+                                    message : "Review Deleted successfully!"
+                                })
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        })
+                }
+                else{
+                    res.status(400).send({
+                        message : "Permission Denied!"
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
